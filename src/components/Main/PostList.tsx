ab5b95fa756a820f181b1f6853170deb09492dd4
dkmqflx <dkmqflx@gmail.com>
2022-05-24 15:22:27 +0900

    포스트 아이템 링크 연결 및 게시글 페이지 생성하기

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -34,11 +34,20 @@ const PostList: FunctionComponent<PostListProps> = function ({
     selectedCategory,
     posts,
   )
+
   return (
     <PostListWrapper ref={containerRef}>
-      {postList.map(({ node: { id, frontmatter } }: PostListItemType) => (
-        <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
-      ))}
+      {postList.map(
+        ({
+          node: {
+            id,
+            fields: { slug },
+            frontmatter,
+          },
+        }: PostListItemType) => (
+          <PostItem {...frontmatter} link={slug} key={id} />
+        ),
+      )}
     </PostListWrapper>
   )
 }
