166ae63bf955dd597d4e08af435af0354232bc56
dkmqflx <dkmqflx@gmail.com>
2022-05-31 11:27:30 +0900

    Canonical Link Element 추가하기

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,5 +80,13 @@ module.exports = {
         ],
       },
     },
+
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: '<https://my-website.com/>',
+        stripQueryString: true,
+      },
+    },
   ],
 }
